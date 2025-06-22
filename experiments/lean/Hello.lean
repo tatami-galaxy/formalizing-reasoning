b34179b7675e46750285999599db5df966a7ca20
Ujan Deb <z5652395@L-G06KKX0RX9.local>
2025-06-22 14:44:02 +1000

    lean

---
+++ experiments/lean/Hello.lean
@@ -0,0 +1,4 @@
+def main : IO Unit :=
+  IO.println "Hello World!"
+
+#check List
---
+++ experiments/lean/scratchpad.lean
@@ -0,0 +1,29 @@
+#eval String.append "Hello" " Lean"
+#eval String.append "it is " (if 1 > 2 then "yes" else "no")
+#eval (1 + 2 : Nat)
+#eval (1 - 2 : Int)
+
+def add1 (n : Nat) : Nat := n + 1
+
+#eval add1 5
+
+def spaceBetween (before : String) (after : String) : String :=
+  String.append before (String.append " " after)
+
+#eval spaceBetween "hello" "world"
+
+#check spaceBetween
+
+structure Point where
+  x : Float
+  y : Float
+deriving Repr
+
+def origin : Point := { x := 0.0, y := 0.0 }
+
+#eval origin.x
+
+def addPoints (p1 : Point) (p2 : Point) : Point :=
+  { x := p1.x + p2.x, y := p1.y + p2.y }
+
+#eval addPoints { x := 1.5, y := 32 } { x := -8, y := 0.2 }
