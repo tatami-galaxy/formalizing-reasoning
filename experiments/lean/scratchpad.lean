1e1cf63b5188f1341a694374e4b09361dfcb98b9
Ujan Deb <z5652395@L-G06KKX0RX9.local>
2025-06-25 22:01:20 +1000

    learning lean

--- experiments/lean/scratchpad.lean
+++ experiments/lean/scratchpad.lean
@@ -27,3 +27,17 @@ def addPoints (p1 : Point) (p2 : Point) : Point :=
   { x := p1.x + p2.x, y := p1.y + p2.y }
 
 #eval addPoints { x := 1.5, y := 32 } { x := -8, y := 0.2 }
+
+
+
+
+
+
+set_option linter.unusedVariables false
+---
+variable {p : Prop}
+variable {q : Prop}
+
+theorem t1 : p → q → p := fun hp : p => fun hq : q => hp
+
+#print t1
